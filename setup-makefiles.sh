a43a142abad7ae3597dd08622b2f7895bbce7603
PlayfulGod <playfulgod@gmail.com>
2013-04-24 10:59:36 -0400

    remove unused libnv.so

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -24,9 +24,7 @@ MAKEFILE=../../../$OUTDIR/$DEVICE-vendor-blobs.mk
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES := \\
-    $OUTDIR/proprietary/lib/liboncrpc.so:obj/lib/liboncrpc.so \\
-    $OUTDIR/proprietary/lib/libnv.so:obj/lib/libnv.so \\
-    $OUTDIR/proprietary/lib/libnv8.so:obj/lib/libnv8.so \\
+    $OUTDIR/proprietary/lib/libv8.so:obj/lib/libv8.so \\
 
 PRODUCT_COPY_FILES += \\
 EOF
