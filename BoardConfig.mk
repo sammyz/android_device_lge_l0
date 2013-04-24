b3081e5b4a509456f0f3d5af8798e1dc8ca8f6e2
PlayfulGod <playfulgod@gmail.com>
2013-04-24 07:11:55 -0400

    first blablabla

---
+++ BoardConfig.mk
@@ -0,0 +1,127 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+
+# Flags
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DLGE_MSM8960
+
+# Include path
+TARGET_SPECIFIC_HEADER_PATH := device/lge/l0/include
+
+# test
+TARGET_NO_BOOTLOADER := true
+
+# Kernel
+BOARD_KERNEL_BASE := 0x80200000
+BOARD_KERNEL_PAGE_SIZE := 2048
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=l0 lpj=676678
+BOARD_FORCE_RAMDISK_ADDRESS := 0x81500000
+
+TARGET_KERNEL_SOURCE := device/lge/MS770_kernel
+TARGET_KERNEL_CONFIG := l0-perf_defconfig
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
+
+# Platform
+TARGET_BOARD_PLATFORM := msm8960
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_PRODUCT := l0_MPCS_US
+
+# Architecture
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Krait optimizations
+TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
+TARGET_USE_KRAIT_PLD_SET := true
+TARGET_KRAIT_BIONIC_PLDOFFS := 10
+TARGET_KRAIT_BIONIC_PLDTHRESH := 10
+TARGET_KRAIT_BIONIC_BBTHRESH := 64
+TARGET_KRAIT_BIONIC_PLDSIZE := 64
+
+# QCOM hardware
+BOARD_USES_QCOM_HARDWARE := true
+
+# Graphics
+COMMON_GLOBAL_CFLAGS += -DQCOM_NO_SECURE_PLAYBACK -DQCOM_ROTATOR_KERNEL_FORMATS
+USE_OPENGL_RENDERER := true
+TARGET_NO_HW_VSYNC := true
+TARGET_USES_C2D_COMPOSITION := true
+TARGET_USES_ION := true
+BOARD_EGL_CFG := device/lge/l0/prebuilt/lib/egl/egl.cfg
+
+# PMEM compatibility
+BOARD_NEEDS_MEMORYHEAPPMEM := true
+
+# ICS proprietary blob compatibility
+COMMON_GLOBAL_CFLAGS += -DICS_CAMERA_BLOB
+
+# Audio
+BOARD_USES_ALSA_AUDIO := true
+TARGET_USES_ION_AUDIO := true
+
+# Lights
+TARGET_PROVIDES_LIBLIGHTS := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+TARGET_CUSTOM_BLUEDROID := ../../../device/lge/l0/bluetooth/bluetooth.c
+
+
+# Wifi
+WIFI_DRIVER_MODULE_NAME          := wlan
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/wlan.ko"
+WPA_SUPPLICANT_VERSION           := VER_0_8_X
+BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
+BOARD_HOSTAPD_DRIVER             := NL80211
+BOARD_WLAN_DEVICE                := qcwcn
+WIFI_DRIVER_FW_PATH_STA          := "sta"
+WIFI_DRIVER_FW_PATH_AP           := "ap"
+WIFI_DRIVER_FW_PATH_P2P          := "p2p"
+
+
+# Filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16776704
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1744829440
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2550136320
+BOARD_FLASH_BLOCK_SIZE := 131072
+BOARD_VOLD_MAX_PARTITIONS := 97
+
+# Webkit
+ENABLE_WEBGL := true
+TARGET_FORCE_CPU_UPLOAD := true
+
+# Preload bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+
+# Vold
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun0/file
+
---
+++ cm.mk
@@ -0,0 +1,36 @@
+# Specify phone tech before including full_phone
+#$(call inherit-product, vendor/cm/config/cdma.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := l0
+
+TARGET_BOOTANIMATION_NAME := vertical-320x480
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/l0/device_l0.mk)
+
+# Device naming
+PRODUCT_DEVICE := l0
+PRODUCT_NAME := cm_l0
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := LG-MS770
+PRODUCT_MANUFACTURER := lge
+
+    CDMA_CARRIER_ALPHA := MetroPCS
+    CDMA_CARRIER_NUMERIC := 311660
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.cdma.home.operator.alpha=$(CDMA_CARRIER_ALPHA) \
+    ro.cdma.home.operator.numeric=$(CDMA_CARRIER_NUMERIC) \
+    gsm.sim.operator.alpha=$(CDMA_CARRIER_ALPHA) \
+    gsm.sim.operator.numeric=$(CDMA_CARRIER_NUMERIC) \
+    gsm.operator.alpha=$(CDMA_CARRIER_ALPHA) \
+    gsm.operator.numeric=$(CDMA_CARRIER_NUMERIC) \
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="lge/l0_MPCS_US/l0:4.1.2/JZO54K/MS77010f.1360114527:user/release-keys" \
+    PRIVATE_BUILD_DESC="l0_MPCS_US-user 4.1.2 JZO54K MS77010f.1360114527 release-keys"
